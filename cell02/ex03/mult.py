48d4569a801359603729a0a8410d120c04cdd475
ksirisil <kayyratnsirisilp@gmail.com>
2025-06-18 03:41:39 +0000

    2.3ใหม่ใหม่ mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,5 +1,9 @@
-num = float(input("Give me a number: "))
-if int(num) == num:
-    print("This number is an integer")
+num1 = int(input("Enter the first nunber:"))
+num2 = int(input("Enter the second nunber:"))
+print(f"{num1} x {num2} = {num1 * num2}")
+if num1 * num2 > 0:
+	print("The result is positive.")
+elif num1 * num2 < 0:
+	print("The result is negative.")
 else:
-    print("This number is a decimal")
+	print("The result is positive and negative.")
